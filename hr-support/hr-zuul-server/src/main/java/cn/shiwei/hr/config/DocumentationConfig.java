ba79aeaa9a321193cf99ab3b14b54253a9687e56
shiwei <shiwei991202@gmail.com>
2021-11-21 21:12:38 +0800

    搭建前端框架, 展示机构类型表数据

---
+++ hr-support/hr-zuul-server/src/main/java/cn/shiwei/hr/config/DocumentationConfig.java
@@ -0,0 +1,30 @@
+package cn.shiwei.hr.config;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Primary
+public class DocumentationConfig implements SwaggerResourcesProvider {
+    @Override
+    public List<SwaggerResource> get() {
+       List resources = new ArrayList<>();
+       // TODO 添加不同微服的swagger 用于聚合
+        // location 只修改 /**/**/v2/api-docs (/前缀/服务别名/固定)
+       resources.add(swaggerResource("系统管理中心", "/hr/system/v2/api-docs", "2.0"));
+       return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location, String version) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion(version);
+        return swaggerResource;
+    }
+}
---
+++ hr-system/hr-service-system/src/main/java/cn/shiwei/hr/config/MybatisPlusConfig.java
@@ -0,0 +1,22 @@
+package cn.shiwei.hr.config;
+
+import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+//Spring boot方式
+@Configuration
+@MapperScan("cn.shiwei.hr.mapper") //mapper接口扫描
+@EnableTransactionManagement  //开启事务管理
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
